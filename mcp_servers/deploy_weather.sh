cc371add85386fce5a5617d26362c71959a4bdb1
Liz raymond <lizraymond@google.com>
2025-11-06 18:32:54 +0000

    updated relative paths and removed iam binding

--- mcp_servers/deploy_weather.sh
+++ mcp_servers/deploy_weather.sh
@@ -7,13 +7,14 @@ source "$SCRIPT_DIR/setup_env.sh"
 export SERVICE_NAME='weather-remote-mcp-server'
 
 gcloud run deploy $SERVICE_NAME \
-  --source ./weather_mcp_server \
+  --source $SCRIPT_DIR/weather_mcp_server \
   --region $LOCATION \
   --project $PROJECT_ID \
   --memory 4G \
   --no-allow-unauthenticated
 
-gcloud run services add-iam-policy-binding $SERVICE_NAME \
-    --member="serviceAccount:service-1077649599081@gcp-sa-aiplatform-re.iam.gserviceaccount.com" \
-    --role="roles/run.invoker" \
-    --region="$LOCATION"
+## MOVE BELOW TO LATER PART OF SCRIPT SINCE SA DOESN'T EXIST YET - done 
+# gcloud run services add-iam-policy-binding $SERVICE_NAME \
+#     --member="serviceAccount:service-$PROJECT_NUMBER@gcp-sa-aiplatform-re.iam.gserviceaccount.com" \
+#     --role="roles/run.invoker" \
+#     --region="$LOCATION"
